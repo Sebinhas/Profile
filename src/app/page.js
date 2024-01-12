4a8122e243fd6b4d7938748d127aa8694c2e32ec
Sebinhas <jsebascuervo05@gmail.com>
2024-01-11 22:24:15 -0500

    deploy

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      hola soy la page
+      </div>
+  )
+}

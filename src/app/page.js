724834c5f8a6d0ed55ea9f31adf4694641fc84e1
Sebinhas <jsebascuervo05@gmail.com>
2024-01-11 22:40:45 -0500

    name

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      hola soy la page
-      </div>
+    <div className="w-full h-screen flex justify-center items-center bg-rose-500 text-slate-800 text-6xl font-black">
+      
+      <div className='drop-shadow-md'> Sebastian Cuervo</div>
+     
+    </div>
   )
 }
